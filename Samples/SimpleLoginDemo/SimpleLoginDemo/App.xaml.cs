44a8b8f57b72503afb9bb5bb8a6d528e20c37225
averrunci <averrunci@fievus.com>
2023-04-14 14:24:08 +0900

    Update sample projects.

--- Samples/SimpleLoginDemo/SimpleLoginDemo/App.xaml.cs
+++ Samples/SimpleLoginDemo/SimpleLoginDemo/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2022 Fievus
+﻿// Copyright (C) 2022-2023 Fievus
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
@@ -6,6 +6,7 @@ using Charites.Windows.Mvc;
 using Charites.Windows.Samples.SimpleLoginDemo.Presentation;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 
 namespace Charites.Windows.Samples.SimpleLoginDemo;
 
@@ -30,7 +31,8 @@ public partial class App
             Content = new ContentControl
             {
                 Content = new SimpleLoginDemoHost()
-            }
+            },
+            SystemBackdrop = new MicaBackdrop()
         };
         windowProvider.Register(window);
         window.Activate();
