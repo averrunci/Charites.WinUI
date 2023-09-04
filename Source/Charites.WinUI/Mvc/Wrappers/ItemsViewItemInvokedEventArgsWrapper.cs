f11a9d4a60d7622d01adda5e0bd39c10fcb622f8
averrunci <averrunci@fievus.com>
2023-09-04 15:43:54 +0900

    Add the ItemsViewItemInvokedEventArgsWrapper class.

---
+++ Source/Charites.WinUI/Mvc/Wrappers/ItemsViewItemInvokedEventArgsWrapper.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) 2023 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+using Microsoft.UI.Xaml.Controls;
+
+namespace Charites.Windows.Mvc.Wrappers;
+
+/// <summary>
+/// Provides data of the <see cref="ItemsViewItemInvokedEventArgs"/>
+/// resolved by <see cref="IItemsViewItemInvokedEventArgsResolver"/>.
+/// </summary>
+public static class ItemsViewItemInvokedEventArgsWrapper
+{
+    /// <summary>
+    /// Gets or sets the <see cref="IItemsViewItemInvokedEventArgsResolver"/>
+    /// that resolves data of the <see cref="ItemsViewItemInvokedEventArgs"/>.
+    /// </summary>
+    public static IItemsViewItemInvokedEventArgsResolver Resolver { get; set; } = new DefaultItemsViewItemInvokedEventArgsResolver();
+
+    /// <summary>
+    /// Gets the invoked item.
+    /// </summary>
+    /// <param name="e">The requested <see cref="ItemsViewItemInvokedEventArgs"/>.</param>
+    /// <returns>The invoked item</returns>
+    public static object? InvokedItem(this ItemsViewItemInvokedEventArgs e) => Resolver.InvokedItem(e);
+
+    private sealed class DefaultItemsViewItemInvokedEventArgsResolver : IItemsViewItemInvokedEventArgsResolver
+    {
+        object? IItemsViewItemInvokedEventArgsResolver.InvokedItem(ItemsViewItemInvokedEventArgs e) => e.InvokedItem;
+    }
+}
+
+/// <summary>
+/// Resolves data of the <see cref="ItemsViewItemInvokedEventArgs"/>.
+/// </summary>
+public interface IItemsViewItemInvokedEventArgsResolver
+{
+    /// <summary>
+    /// Gets the invoked item.
+    /// </summary>
+    /// <param name="e">The requested <see cref="ItemsViewItemInvokedEventArgs"/>.</param>
+    /// <returns>The invoked item</returns>
+    object? InvokedItem(ItemsViewItemInvokedEventArgs e);
+}
